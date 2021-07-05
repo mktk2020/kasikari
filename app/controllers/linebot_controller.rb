1adf8f69ace039c0452d6092e5d6625b903d17df
a <akiizu.2612@gmail.com>
2021-07-05 10:09:18 +0900

    line_bot文字により変更

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,13 +12,25 @@ class LinebotController < ApplicationController
       events = client.parse_events_from(body)
   
       events.each do |event|
+        
+        # event.message['text']でLINEで送られてきた文書を取得
+      if event.message['text'].include?("好き")
+        response = "すきすきすきすきすきすきすきぃぃぃぃぃ"
+      elsif event.message["text"].include?("行ってきます")
+        response = "どこいくの？どこいくの？どこいくの？寂しい寂しい寂しい。。。"
+      elsif event.message['text'].include?("おはよう")
+        response = "おはよう。なんで今まで連絡くれなかったの？"
+      else
+        response = @post.name
+      end
+
         case event
         when Line::Bot::Event::Message
           case event.type
           when Line::Bot::Event::MessageType::Text
             message = {
               type: 'text',
-              text: event.message['text']
+              text: response
             }
           end
         end
