50eabc30aa9fd1cfe037b2cfa2a03d35ed072429
m-sugimoto <mix.rix@icloud.com>
2021-07-09 15:33:32 +0900

    貸し出し商品登録エラーチェック追加

--- app/models/lendingitem.rb
+++ app/models/lendingitem.rb
@@ -1,2 +1,10 @@
 class Lendingitem < ApplicationRecord
+    validates :item, presence: {message: '商品名を入力してください'}
+    validates :category, presence: {message: 'カテゴリーよりお選びください。'}
+    #validates :image_name, presence: {message: '商品画像を挿入してください。'} 
+    validates :price, presence: {message: '1日当たりの単価を入力してください。'}
+    validates :days, presence: {message: '最長レンタル可能日数を入力してください。'}
+
+
+
 end
