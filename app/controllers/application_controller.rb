a064a13a95ee62fef83e3b6d0e8308b7d9fbdd93
ochiai1 <manmei2013@gmail.com>
2021-07-16 14:26:07 +0900

    account変更後 マイページへ飛ぶようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
+    
     def after_sign_out_path_for(resource)
          '/hello/index'
     end
     def after_sign_in_path_for(resource)
         '/hello/login_check'
     end
-   
+  
     # deviseのコントローラを使う前に呼ばれるアクション
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+ 
     private
-      def configure_permitted_parameters
+    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:u_name,:prefecture])
       devise_parameter_sanitizer.permit(:account_update, keys: [:u_name,:prefecture])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'hello/index'
   get 'hello/login_check'
 
+  root :to =>  "hello#login_check"
   devise_for :accounts
   
   resources :lendingitems
